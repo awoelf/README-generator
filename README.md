a653bdc3102baefb8d7c3231e9b1633900cdffa5
awoelf <98348782+awoelf@users.noreply.github.com>
2022-10-25 00:48:55 -0500

    feature complete

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # README-generator
 A handy script for generating a beautiful README.md file.
 
+![GitHub](https://img.shields.io/github/license/awoelf/weather-dashboard)
--- index.js
+++ index.js
@@ -1,86 +1,157 @@
 // TODO: Include packages needed for this application
-const fs = require("fs");
-const inquirer = require("inquirer");
-
-// TODO: Create an array of questions for user input
-const questions = [""];
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-  fs.writeFile(`${fileName}`, readmeContent, (err) =>
-    err ? console.log(err) : console.log("Successfully created README file.")
+function writeToFile(fileName, fileContent) {
+  fs.writeFile(`${fileName}`, fileContent, (err) =>
+    err ? console.log(err) : console.log('Successfully created README file.')
   );
 }
+
+const readmeData = ({
+  title,
+  repo,
+  description,
+  installation,
+  usage,
+  license,
+  contributing,
+  tests,
+  username,
+  email,
+  questions,
+}) =>
+  `<a name='readme-top'></a>
+  ![GitHub](https://img.shields.io/github/license/${username}/${repo})
+  # ${title}
+
+  ### Table of Contents
+  <ol>
+  <li><a href='#description'>Description</a></li>
+  <li><a href='#installation'>Installation</a></li>
+  <li><a href='#usage'>Usage</a></li>
+  <li><a href='#license'>License</a></li>
+  <li><a href='#contributing'>Contributing</a></li>
+  <li><a href='#description'>Tests</a></li>
+  <li><a href='#description'>Questions</a></li>
+  </ol>
+
+  ## Description
+  ${description}
+
+  ## Installation
+  ${installation}
+
+  ## Usage
+  ${usage}
+
+  ## License
+  Distributed unter the ${license} license. See \`LICENSE.txt\` for more information.
+  
+  ## Contributing
+  ${contributing}
+
+  ## Tests
+  ${tests}
+
+  ## Questions
+  ${questions}
+  GitHub - [${username}](https://github.com/${username})
+  Email - [${email}](mailto:${email})
+  <p align="right">(<a href="#readme-top">back to top</a>)</p>`;
 
 // TODO: Create a function to initialize app
 function init() {
   inquirer
     .prompt([
       {
-        type: "confirm",
-        message: "Would you like to create a README.md file?",
-        name: "answer",
+        type: 'confirm',
+        message: 'Would you like to create a README.md file?',
+        name: 'answer',
       },
     ])
     .then((response) => {
       if (response.answer) {
-        readmeStart();
+        readmePrompts();
       } else {
-        console.log("Closing app.");
+        console.log('Closing app.');
       }
     });
 }
 
-function readmeStart() {
+function readmePrompts() {
   console.log(
-    "Please enter the information you would like to put inside each section of the readme.\nIf do not want to include a section, type 'none' when prompted."
+    'Please enter the information you would like to put inside each section of the readme.'
   );
-  inquirer.prompt([
-    {
-      type: "input",
-      //   message: "What is the title of the project?",
-      message: console.log("test"),
-      name: "title",
-    },
-    {
-      type: "input",
-      message: "What is the description of the project?",
-      name: "description",
-    },
-    {
-      type: "input",
-      message: "How do you install the project?",
-      name: "installation",
-    },
-    {
-      type: "input",
-      message: "How do people use the project?",
-      name: "usage",
-    },
-    {
-      type: "input",
-      message: "Which license are you using?",
-      name: "license",
-    },
-    {
-      type: "input",
-      message: "What can people do if they want to contribute to the project?",
-      name: "contributing",
-    },
-    {
-      type: "input",
-      message: "What can people do to test the project?",
-      name: "tests",
-    },
-    {
-      type: "input",
-      message: "What are common questions and answers about the project?",
-      name: "questions",
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'What is the title of the project?',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'What is the name of the repo?',
+        name: 'repo',
+      },
+      {
+        type: 'input',
+        message: 'What is the description of the project?',
+        name: 'description',
+      },
+      {
+        type: 'input',
+        message: 'How do you install the project?',
+        name: 'installation',
+      },
+      {
+        type: 'input',
+        message: 'What are the usage instructions?',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Which license are you using?',
+        name: 'license',
+      },
+      {
+        type: 'input',
+        message:
+          'What can people do if they want to contribute to the project?',
+        name: 'contributing',
+      },
+      {
+        type: 'input',
+        message: 'What are the testing instructions for the project?',
+        name: 'tests',
+      },
+      {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username',
+      },
+      {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+      },
+      {
+        type: 'input',
+        message: 'How should people reach you for questions?',
+        name: 'questions',
+      },
+    ])
+    .then((data) => {
+      const fileName = `./readme_files/README_${data.title
+        .toLowerCase()
+        .split(' ')
+        .join('')}.md`;
+      const fileContent = readmeData(data);
+      writeToFile(fileName, fileContent);
+    });
 }
-
-function generateReadme(data) {}
 
 // Function call to initialize app
 init();
